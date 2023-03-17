8d3f783e877b49ccafc284c147f0e19e3c640b30
emynrg <mohamedamine.regaia@esprit.tn>
2023-03-17 23:47:44 +0100

    Initial Commit

---
+++ MiniProjet_IOS/MiniProjet_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniProjet_IOSApp.swift
+//  MiniProjet_IOS
+//
+//  Created by Mohamed amine Regaia on 17/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct MiniProjet_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
